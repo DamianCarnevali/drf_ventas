868745a4d56570691ff3c1eb2b479d8b6bfcec24
DamianCarnevali <121942474+DamianCarnevali@users.noreply.github.com>
2024-06-24 16:01:38 -0300

    le pongo el settings directamente

--- inventarios/inventarios/wsgi.py
+++ inventarios/inventarios/wsgi.py
@@ -1,6 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from inventarios.inventarios import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventarios.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
 
 application = get_wsgi_application()
