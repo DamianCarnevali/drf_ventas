59bb75d4dc1584a7c6b628a6e584d60a6e275070
DamianCarnevali <121942474+DamianCarnevali@users.noreply.github.com>
2024-06-24 14:38:50 -0300

    cambio de requirements

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 # Exit on error
 set -o errexit
 
-#Navega hacia la ruta correcta del manage.py
-cd /opt/render/project/src/inventarios
-
 # Instala en el servidor los paquetes que figuran en requirements
 pip install -r requirements.txt
+
+#Navega hacia la ruta correcta del manage.py
+cd /opt/render/project/src/inventarios
 
 # Convierte los archivos estáticos (DRF usa diseño y navegación por defecto)
 python manage.py collectstatic --no-input
