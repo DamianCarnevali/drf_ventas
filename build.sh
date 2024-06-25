2a5a85770f5cb46ae50e2792cb21263127ecb10b
DamianCarnevali <121942474+DamianCarnevali@users.noreply.github.com>
2024-06-25 14:26:20 -0300

    cambios en build

--- build.sh
+++ build.sh
@@ -5,9 +5,6 @@ set -o errexit
 # Instala en el servidor los paquetes que figuran en requirements
 pip install -r requirements.txt
 
-#Navega hacia la ruta correcta del manage.py
-cd /opt/render/project/src/inventarios
-
 # Convierte los archivos estáticos (DRF usa diseño y navegación por defecto)
 python manage.py collectstatic --no-input
 
